98917638938aa19ff5fbdef0f0cf338cd0117967
Lukas A. Santiago <lukas.asant@gmail.com>
2022-11-20 02:38:32 -0300

    SimuladorDoFirmware

---
+++ SimuladorDoFirmware/GuidHelper.cs
@@ -0,0 +1,24 @@
+﻿
+// Busca Configuração
+// Se não encontra utiliza a antiga
+// Seta a configuração
+// Executa a Configuração
+
+
+public static class GuidHelper
+{
+    public static string ToShortString(Guid guid)
+    {
+        var base64Guid = Convert.ToBase64String(guid.ToByteArray());
+        base64Guid = base64Guid.Replace('+', '-').Replace('/', '_');
+        // Remove the trailing ==
+        return base64Guid.Substring(0, base64Guid.Length - 2);
+    }
+
+    public static Guid FromShortString(string str)
+    {
+        str = str.Replace('_', '/').Replace('-', '+');
+        var byteArray = Convert.FromBase64String(str + "==");
+        return new Guid(byteArray);
+    }
+}
---
+++ SimuladorDoFirmware/Program.cs
@@ -0,0 +1,14 @@
+﻿
+string ApiKey = "MinhaApiKeyTop";
+Guid UniqueDeviceId = Guid.Parse("fd8d872d-3045-47c8-8b24-9db2d4807b7c"); //Guid.NewGuid();
+string shortUniqueDeviceId = GuidHelper.ToShortString(UniqueDeviceId); 
+
+
+Console.WriteLine("Guid: " + UniqueDeviceId.ToString());
+Console.WriteLine("Short Guid: " + shortUniqueDeviceId);
+Console.WriteLine("ApiKey: " + ApiKey);
+
+// Busca Configuração
+// -> Se não encontra utiliza a antiga
+// Seta a configuração
+// Executa a Configuração
